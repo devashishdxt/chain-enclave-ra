d072087139f819abedb48f374497208022cc8a1c
Devashish Dixit <devashishdxt@gmail.com>
2020-03-18 03:54:18 +0000

    Add client to connect to SP server for remote attestation

--- ra-sp/src/lib.rs
+++ ra-sp/src/lib.rs
@@ -3,6 +3,10 @@
 //!
 //! # Usage
 //!
+//! This crate can be used in two modes: `server` and `client` which can be enabled by their respective Cargo features.
+//!
+//! ## Running SP server for remote attestation (`"server"` feature must be enabled)
+//!
 //! ```rust,no_run
 //! use ra_sp::{SpRaConfig, SpRaServer};
 //!
@@ -12,13 +16,41 @@
 //! let server = SpRaServer::new(config).unwrap();
 //! server.run(address).unwrap();
 //! ```
+//!
+//! ## Calling SP server to get attestation report (`"client"` feature must be enabled)
+//!
+//! ```rust,no_run
+//! use ra_sp::SpRaClient;
+//!
+//! let address = "0.0.0.0:8989";
+//! let client = SpRaClient::connect(address).unwrap();
+//!
+//! let target_info = client.get_target_info().unwrap();
+//!
+//! // Generate a enclave report using received target info
+//! let report = vec![];
+//!
+//! let quote_result = client.get_quote(report).unwrap();
+//!
+//! // Verify the QE report in `quote_result`
+//! let attestation_report = client.get_attestation_report(quote_result.quote).unwrap();
+//! ```
+#[cfg(feature = "client")]
+pub mod client;
+#[cfg(feature = "server")]
 pub mod config;
+#[cfg(feature = "server")]
 pub mod context;
+#[cfg(feature = "server")]
 pub mod ias_client;
 pub mod protocol;
+#[cfg(feature = "server")]
 pub mod server;
 pub mod types;
 
+#[cfg(feature = "client")]
+pub use self::client::SpRaClient;
+#[cfg(feature = "server")]
 pub use self::{
     config::SpRaConfig,
     server::{SpRaServer, SpRaServerError},
--- ra-sp/src/types.rs
+++ ra-sp/src/types.rs
@@ -1,9 +1,11 @@
+#[cfg(feature = "server")]
 mod attestation_evidence;
 mod attestation_report;
 mod quote_result;
 
+#[cfg(feature = "server")]
+pub use self::attestation_evidence::AttestationEvidence;
 pub use self::{
-    attestation_evidence::AttestationEvidence,
     attestation_report::{AttestationReport, AttestationReportBody},
     quote_result::QuoteResult,
 };
